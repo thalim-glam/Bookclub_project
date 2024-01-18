0384cc23ae9e59d0a11ab1b655b19db7d072ca29
Tasneem <thalim.glam@gmail.com>
2024-01-18 09:01:32 -0800

    Updating this repo. Because accidentally I submitted this repo link as my project 2 submission

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,6 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the root/homepage
         document.location.replace('/');
       } else {
         alert(response.statusText);
@@ -35,7 +34,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
